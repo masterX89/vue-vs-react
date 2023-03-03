1f602365933125ec09d3c4c22ec12b7bc75d81a5
masterX89 <xhk1995@gmail.com>
2023-03-03 11:29:11 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-vs-react
+
+用于记录学习 vue 和 react 源码过程中的区别
---
+++ packages/index.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest'
+
+describe('HelloWorld', () => {
+  it('renders properly', () => {
+    expect(1).toBe(1)
+  })
+})
